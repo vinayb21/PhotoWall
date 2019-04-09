d629e8eaee4ea9c3a31b6cd045953f3e153d6c23
vinayb21 <vinay@hackerrank.com>
2019-04-09 10:09:58 +0530

    Use map to iterate over an array and show the list items

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import ReactDOM from 'react-dom';
 const tasks = ['Take out the trash', 'Shovel the driveway', 'Walk the dog'];
 
 const element = <ol>
-  <li>{tasks[0]}</li>
-  <li>{tasks[1]}</li>
-  <li>{tasks[2]}</li>
+  { tasks.map((task) => <li> {task} </li>)}
   </ol>;
 
 ReactDOM.render(element, document.getElementById('root'));
