52a9d88cc7d33caf1372de8a07c90e9193197f17
vinayb21 <vinay@hackerrank.com>
2019-04-09 10:17:01 +0530

    Use Enclosing tags in JSX for multiple elements

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 
 const tasks = ['Take out the trash', 'Shovel the driveway', 'Walk the dog'];
 
-const element = <ol>
-  { tasks.map((task) => <li> {task} </li>)}
-  </ol>;
+const element = 
+  <div>
+    <h1> Tasks list </h1>
+    <ol>
+      { tasks.map((task, index) => <li key = {index}> {task} </li>)}
+    </ol>
+  </div>
+
 
 ReactDOM.render(element, document.getElementById('root'));
