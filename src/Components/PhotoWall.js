86744fc6d997cf15e112d879f5669a0a8c91aa7f
vinayb21 <vinay@hackerrank.com>
2019-04-10 10:17:00 +0530

    Functional components

--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -1,12 +1,10 @@
 import React, {Component} from 'react';
 import Photo from './Photo';
 
-class PhotoWall extends Component {
-  render() {
-    return <div className="photoGrid">
-      {this.props.posts.map((post, index) => <Photo key={index} post={post}/>)}
-    </div>
-  }
+function PhotoWall(props) {
+  return <div className="photoGrid">
+          {props.posts.map((post, index) => <Photo key={index} post={post}/>)}
+        </div>  
 }
 
 export default PhotoWall
