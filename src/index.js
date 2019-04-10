6a39f4cbd272ddf1801cb2f1cbe2087b0246ec6a
vinayb21 <vinay@hackerrank.com>
2019-04-10 10:12:06 +0530

    App setup and component architecture

--- src/index.js
+++ src/index.js
@@ -1,34 +1,6 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-
-class List extends Component {
-  render() {
-    return (
-      <ol>
-        { this.props.tasks.map((task, index) => <li key = {index}> {task} </li>)}
-      </ol>
-    )
-  }
-}
-
-class Title extends Component {
-  render() {
-    return (
-      <h1> {this.props.title} </h1>
-    )
-  }
-}
-
-class Main extends Component {
-  render() {
-    return (
-      <div>
-        <Title title={'TODOS:'}/>
-        <List tasks={['Mow the lawn', 'Walk the dog']}/>
-        <List tasks={['Hose the driveway', 'Finish the laundry']}/>
-      </div>
-    )
-  }
-}
+import Main from './Components/Main'
+import './Styles/stylesheet.css';
 
 ReactDOM.render(<Main/>, document.getElementById('root'));
